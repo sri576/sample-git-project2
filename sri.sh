42bdefaa2f11a5461868a43282b7b3b3dded44e4
root <root@ip-172-31-39-243.ap-south-1.compute.internal>
2022-11-03 11:40:09 +0000

    developer created this files

--- sri.sh
+++ sri.sh
@@ -2,8 +2,8 @@ git init
 mkdir srikanth
 
 touch file1
-cp /home/sri.sh/file1 /home/file2
-cp /home/sri.sh/file1 /home/file2
+cp /root/sri/file1 /root/srikanth/file2
+cp /root/sri/file1 /root/srikanth/file3
 git add .
 git commit -m 'developer created this files '
 
